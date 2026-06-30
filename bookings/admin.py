32d568f691fc4e9f6118699bc14e90fda23e54ad
Htet Myo Aung <htetmyoaung37@gmail.com>
2026-06-30 23:11:12 +0900

    feat: enhance django admin interface with list_display, filters, and search fields

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User, Room, RoomCombination, Reservation
 
 
-admin.site.register(User)
-admin.site.register(Room)
-admin.site.register(RoomCombination)
-admin.site.register(Reservation)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('phone_number', 'role', 'birthdate')}),
+    )
+    list_display = ['username', 'email', 'role', 'is_staff']
+
+
+admin.site.register(User, CustomUserAdmin)
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('room_name', 'capacity_single')
+    search_fields = ('room_name',)
+
+
+@admin.register(RoomCombination)
+class RoomCombinationAdmin(admin.ModelAdmin):
+    list_display = ('combination_name', 'max_capacity')
+    search_fields = ('combination_name',)
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'reserved_date', 'reserved_time',
+                    'number_of_guests', 'status')
+    list_filter = ('status', 'reserved_date')
+    search_fields = ('user__username', 'status')
